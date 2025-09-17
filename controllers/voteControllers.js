3d0295ceeb009e199bf299e085adef0e05259b21
intec-inc <connect.inkluzon@gmail.com>
2025-09-18 01:12:55 +0300

    AS

--- controllers/voteControllers.js
+++ controllers/voteControllers.js
@@ -13,14 +13,35 @@ exports.getVotesSummary = async (req, res) => {
       return res.json(JSON.parse(cached));
     }
 
+
+      //    SELECT 
+      // c.id AS category_id,
+      // c.name AS category_name,
+      // n.id AS nominee_id,
+      // n.name AS nominee_name,
+      // COUNT(v.id) AS total_votes
+      // FROM nominees n
+      // JOIN categories c ON n.category_id = c.id
+      // LEFT JOIN votes v ON v.candidate_id = n.id
+      // GROUP BY c.id, n.id
+      // ORDER BY c.id, total_votes DESC
+
     // ✅ fetch from MySQL
     const [rows] = await db.promise().query(`
-      SELECT 
-      c.id AS category_id,
-      c.name AS category_name,
-      n.id AS nominee_id,
-      n.name AS nominee_name,
-      COUNT(v.id) AS total_votes
+       SELECT 
+       c.id AS category_id,
+       c.name AS category_name,
+       n.id AS nominee_id,
+       n.name AS nominee_name,
+       COUNT(v.id) AS total_votes,
+       ROUND(
+       (COUNT(v.id) / NULLIF(
+            (SELECT COUNT(v2.id) 
+             FROM votes v2 
+             JOIN nominees n2 ON v2.candidate_id = n2.id 
+             WHERE n2.category_id = c.id), 0
+          ) * 100), 2
+        ) AS percentage
       FROM nominees n
       JOIN categories c ON n.category_id = c.id
       LEFT JOIN votes v ON v.candidate_id = n.id
