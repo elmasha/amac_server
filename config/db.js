748376e7425346d613c2636c24afbc9954a4b9df
intec-inc <connect.inkluzon@gmail.com>
2025-09-17 03:04:55 +0300

    DF

--- config/db.js
+++ config/db.js
@@ -1,7 +1,19 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+const mysql = require("mysql2");
+const mysqlPromise = require("mysql2/promise");
+require("dotenv").config();
+
 const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
+
+const pool = mysqlPromise.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
@@ -17,7 +29,15 @@ connection.connect((err) => {
   console.log('✅ MySQL connected successfully');
 });
 
-module.exports = connection;
+pool.connect((err) => {
+  if (err) {
+    console.error('❌ pool connection failed:', err.message);
+    return;
+  }
+  console.log('✅ pool connected successfully');
+});
+
+module.exports = {  connection, pool };
 
 
 // require('dotenv').config();
